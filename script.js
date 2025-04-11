b2c11df9334de8cfe3e525522d570a36e4eaac97
fdsdigital <maflowers28@gmail.com>
2025-04-11 15:21:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const phone = document.getElementById('phone');
+
+function linkDirections() {
+    window.open('https://www.bing.com/search?q=C%20%26%20C%20Eatery&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=c%20%26%20c%20eatery&sc=12-12&sk=&cvid=245423077E86461C99D208B3EE406295&ghsh=0&ghacc=0&ghpl=');
+}
+
+phone.addEventListener('click', () => {
+    window.open('https://www.bing.com/search?q=C%20%26%20C%20Eatery&qs=n&form=QBRE&sp=-1&ghc=1&lq=0&pq=c%20%26%20c%20eatery&sc=12-12&sk=&cvid=245423077E86461C99D208B3EE406295&ghsh=0&ghacc=0&ghpl=');
+})
